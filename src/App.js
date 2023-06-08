116887b4112836abb21a56dbc8cfe14b15eb5660
B Rohan Rao <109647650+rohanlearn@users.noreply.github.com>
2023-06-08 16:36:28 +0530

    added cart open show feature

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import Header from "./components/Layout/Header";
 import { Fragment } from "react";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import { useState } from "react";
 function App() {
+    const [cartState,changeCartState] = useState(false)
+
+    const cartStateHandler = ()=>{
+        changeCartState(!cartState)
+        console.log(!cartState)
+    }
+
+  
+
   return (
     <>
-    <Cart/>
-      <Header/>
+    {cartState && <Cart cartState={cartStateHandler}/>}
+      <Header cartSState={cartStateHandler}/>
       <main>
       <Meals/>
 
--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,16 +1,21 @@
 import classes from  "./Header.module.css"
-import { Fragment } from "react"
+import { Fragment} from "react"
 import mealsImage from '../assets/meals.jpg'
 import HeaderCartButton from "./HeaderCartButton"
 
 const Header = (props)=>{
+    const handler = ()=>{
+        
+    }
+
+    
 
 
     return(
         <>
         <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton/>
+        <HeaderCartButton onClick={props.cartSState}/>
         </header>
             
             <div className={classes['main-image']}>
